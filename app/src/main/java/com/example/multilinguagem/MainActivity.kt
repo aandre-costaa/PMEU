1c4bdc8ed8948572c69fc81eea655e758c175c04
andre.costa <andre.costa@ipvc.pt>
2022-10-03 12:02:34 +0100

    add update

--- app/src/main/java/com/example/multilinguagem/MainActivity.kt
+++ app/src/main/java/com/example/multilinguagem/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText( this, R.string.welcome, Toast.LENGTH_SHORT).show()
+        //Update
     }
 }
