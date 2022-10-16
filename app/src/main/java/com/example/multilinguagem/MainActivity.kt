45bdce084b9575a7e95a9f0d65f643fb3566b482
andre.costa <andre.costa@ipvc.pt>
2022-10-16 15:30:36 +0100

    add update

--- app/src/main/java/com/example/multilinguagem/MainActivity.kt
+++ app/src/main/java/com/example/multilinguagem/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.multilinguagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Toast.makeText( this, R.string.welcome, Toast.LENGTH_SHORT).show()
         //Update
+    }
+
+    fun button1(view: View){
+
+        var editTextHello = findViewById<EditText>(R.id.editTextTextPersonName)
+        Toast.makeText(this, editTextHello.text, Toast.LENGTH_SHORT).show()
+        findViewById<TextView>(R.id.text1).setText(editTextHello.text)
+    }
+    
+    fun button2(view: View){
+        Toast.makeText(this, "button2", Toast.LENGTH_SHORT).show()
     }
 }
