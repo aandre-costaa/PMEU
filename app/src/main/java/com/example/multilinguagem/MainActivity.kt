4ef4071a7655b79fd83938ebaef13d9dc3de40cc
andre.costa <andre.costa@ipvc.pt>
2022-10-17 12:43:32 +0100

    add update

--- app/src/main/java/com/example/multilinguagem/MainActivity.kt
+++ app/src/main/java/com/example/multilinguagem/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.multilinguagem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+
+const val PARAM1_NAME = "nome"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,12 +23,44 @@ class MainActivity : AppCompatActivity() {
 
     fun button1(view: View){
 
-        var editTextHello = findViewById<EditText>(R.id.editTextTextPersonName)
-        Toast.makeText(this, editTextHello.text, Toast.LENGTH_SHORT).show()
-        findViewById<TextView>(R.id.text1).setText(editTextHello.text)
+        var edit1 = findViewById<EditText>(R.id.editTextTextPersonName)
+        Toast.makeText(this, edit1.text, Toast.LENGTH_SHORT).show()
+        findViewById<TextView>(R.id.text1).setText(edit1.text)
     }
     
     fun button2(view: View){
-        Toast.makeText(this, "button2", Toast.LENGTH_SHORT).show()
+        var edit1 = findViewById<EditText>(R.id.editTextTextPersonName)
+        val intent = Intent(this, MainActivity2::class.java).apply {
+            putExtra(PARAM1_NAME, edit1.text.toString())
+        }
+        startActivity(intent)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.create_new -> {
+                Toast.makeText(this, "create_new", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.opcao2 -> {
+                Toast.makeText(this, "opcao2", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.opcao3 -> {
+                Toast.makeText(this, "opcao3", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.opcao4 -> {
+                Toast.makeText(this, "opcao4", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
